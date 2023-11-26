1a392644a675e954cfec02b416a981918360a231
Othen Prock <othen.prock@gmail.com>
2023-11-26 12:36:22 -0500

    filter works for both is and isnot, but not ready for attributes

--- src/main/java/com/acorn/xmlsnap/controller/MainController.java
+++ src/main/java/com/acorn/xmlsnap/controller/MainController.java
@@ -8,7 +8,7 @@ import com.acorn.xmlsnap.tool.XMLImporter;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,9 +20,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MainController implements Initializable {
 
